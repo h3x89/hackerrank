78499abf8b886f87c36bc9b0a595dac8b28fd556
Robert Kubiś <robert.h3x@gmail.com>
2024-11-26 19:54:51 +0100

    Add jupytext example

---
+++ Python/Jupytext.py
@@ -0,0 +1,22 @@
+# %%
+x = 5
+y = 10
+
+print(x - y)
+# %%
+
+# print(data)
+# %%
+
+text = "Hello!"
+
+def greet(var: str):
+    return(var)
+# %%
+
+
+var1 = greet(text + "1")  # <-- Debugger is here
+var2 = greet(text + "2")  # <-- Debugger is here
+
+
+# %%
