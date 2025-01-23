e54ee665c806e8fc6a5a581278ed54aad6ce4e4f
Robert Kubiś <robert.h3x@gmail.com>
2025-01-23 22:09:42 +0100

    Add unit tests for evaluatePath function: Introduced a suite of unit tests to validate the behavior of the `evaluatePath` function, ensuring accurate calculation of the player's final position and total moves across various input scenarios, including edge cases. This enhances the reliability of the function and confirms its correctness.

--- codesignal/93.3.2.py
+++ codesignal/93.3.2.py
@@ -63,3 +63,27 @@ def evaluatePath(numbers):
     return (position, moves)
 
 print(evaluatePath([3, 4, 1, 1, -3, 1])) # (4, 5)
+
+import unittest
+
+class EvaluatePathTests(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(evaluatePath([2, 1, -3, 4]), (2, 1))
+
+    def test2(self):
+        self.assertEqual(evaluatePath([0]), (0, 0))
+
+    def test3(self):
+        self.assertEqual(evaluatePath([3, 4, 1, 1, -3, 1]), (4, 5))
+
+    def test4(self):
+        self.assertEqual(evaluatePath([3, -3]), (0, 0))
+
+    def test5(self):
+        self.assertEqual(evaluatePath([3, 2, -1, 2, 2, -1, 4]), (1, 7))
+
+    def test6(self):
+        self.assertEqual(evaluatePath([1]*500), (0, 998))    
+
+if __name__ == '__main__':
+    unittest.main()
